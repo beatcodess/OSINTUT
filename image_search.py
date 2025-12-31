50e706aa4043863785257ce5a44a9a1864c3b683
beatcodes <beatcodes@proton.me>
2025-12-30 20:25:32 -0600

    Delete image_search.py

--- image_search.py
+++
@@ -1,9 +0,0 @@
-from duckduckgo_search import DDGS
-
-def search_images(n,c):
-    q=f'"{n}" "{c}"'
-    a=[]
-    with DDGS() as d:
-        for r in d.images(q,max_results=25):
-            a.append(r.get("image"))
-    return a
