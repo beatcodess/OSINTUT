6c4a03c49d7afb656eeea44ca6abec4a27816c56
beatcodes <beatcodes@proton.me>
2025-12-31 19:02:16 -0600

    Update username_search.py

--- modules/username_search.py
+++ modules/username_search.py
@@ -1,113 +1,73 @@
-import asyncio,aiohttp,itertools,hashlib,time,socket
+import requests
 
-PLATFORMS={
-"GitHub":(["https://github.com/{}"],3),
-"GitLab":(["https://gitlab.com/{}"],3),
-"Bitbucket":(["https://bitbucket.org/{}"],3),
-"Twitter":(["https://twitter.com/{}","https://x.com/{}"],2),
-"Instagram":(["https://instagram.com/{}"],2),
-"Facebook":(["https://facebook.com/{}"],1),
-"Reddit":(["https://reddit.com/user/{}","https://old.reddit.com/user/{}"],2),
-"Telegram":(["https://t.me/{}","https://t.me/s/{}"],3),
-"TikTok":(["https://tiktok.com/@{}"],2),
-"Snapchat":(["https://snapchat.com/add/{}"],1),
-"Steam":(["https://steamcommunity.com/id/{}"],2),
-"SoundCloud":(["https://soundcloud.com/{}"],2),
-"Spotify":(["https://open.spotify.com/user/{}"],2),
-"DeviantArt":(["https://deviantart.com/{}"],2),
-"Imgur":(["https://imgur.com/user/{}"],2),
-"Kaggle":(["https://kaggle.com/{}"],3),
-"LeetCode":(["https://leetcode.com/{}"],3),
-"HackerRank":(["https://hackerrank.com/{}"],3),
-"DockerHub":(["https://hub.docker.com/u/{}"],3),
-"PyPI":(["https://pypi.org/user/{}"],3),
-"NPM":(["https://www.npmjs.com/~{}"],3),
-"Medium":(["https://medium.com/@{}"],2),
-"Mastodon":(["https://mastodon.social/@{}"],2),
-"VK":(["https://vk.com/{}"],2),
-"Pinterest":(["https://pinterest.com/{}"],1),
-"Tumblr":(["https://{}.tumblr.com"],2),
-"Linktree":(["https://linktr.ee/{}"],2),
-"Carrd":(["https://{}.carrd.co"],2),
-"TryHackMe":(["https://tryhackme.com/p/{}"],4),
-"HackTheBox":(["https://app.hackthebox.com/users/{}"],4),
-"RootMe":(["https://www.root-me.org/{}"],4),
-"CTFtime":(["https://ctftime.org/user/{}"],4),
-"HackerOne":(["https://hackerone.com/{}"],4),
-"Bugcrowd":(["https://bugcrowd.com/{}"],4),
-"ExploitDB":(["https://www.exploit-db.com/?author={}"],4),
-"Pastebin":(["https://pastebin.com/u/{}"],4),
-"Keybase":(["https://keybase.io/{}"],4),
-"0x00sec":(["https://0x00sec.org/u/{}"],5),
-"HackForums":(["https://hackforums.net/member.php?action=profile&uid={}"],5),
-"Nulled":(["https://nulled.to/user/{}"],5),
-"Cracked":(["https://cracked.io/{}"],5),
-"Breached":(["https://breached.vc/User-{}"],5),
-"XSS":(["https://xss.is/user/{}"],5),
-"Doxbin":(["https://doxbin.org/user/{}","https://doxbin.com/user/{}"],5)
+SITES = {
+    "Facebook": {
+        "url": "https://www.facebook.com/{}",
+        "weight": 0.9
+    },
+    "Instagram": {
+        "url": "https://www.instagram.com/{}/",
+        "weight": 0.95
+    },
+    "Twitter/X": {
+        "url": "https://twitter.com/{}",
+        "weight": 0.9
+    },
+    "TikTok": {
+        "url": "https://www.tiktok.com/@{}",
+        "weight": 0.9
+    },
+    "Reddit": {
+        "url": "https://www.reddit.com/user/{}/",
+        "weight": 0.85
+    },
+    "GitHub": {
+        "url": "https://github.com/{}",
+        "weight": 0.95
+    },
+    "YouTube": {
+        "url": "https://www.youtube.com/@{}",
+        "weight": 0.7
+    },
+    "Pinterest": {
+        "url": "https://www.pinterest.com/{}/",
+        "weight": 0.7
+    },
+    "SoundCloud": {
+        "url": "https://soundcloud.com/{}",
+        "weight": 0.7
+    },
+    "Steam": {
+        "url": "https://steamcommunity.com/id/{}",
+        "weight": 0.6
+    }
 }
 
-CACHE={}
-TTL=3600
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
 
-def _tor_available():
-    try:
-        s=socket.create_connection(("127.0.0.1",9050),2)
-        s.close()
-        return True
-    except:
-        return False
+def find_socials(username, tor=False):
+    results = {}
 
-def _mutations(u):
-    subs={"a":"4","e":"3","i":"1","o":"0","s":"5","t":"7"}
-    out={u,u.lower()}
-    for k,v in subs.items():
-        out.add(u.replace(k,v))
-        out.add(u.replace(k,v).lower())
-    for i in range(30):
-        out.add(f"{u}{i}")
-        out.add(f"{u}_{i}")
-    return out
+    for site, info in SITES.items():
+        profile_url = info["url"].format(username)
 
-def _expand_urls():
-    urls=[]
-    for bases,_ in PLATFORMS.values():
-        for b in bases:
-            urls.append(b)
-    return urls
-
-ALL_URLS=_expand_urls()
-
-async def _check(session,sem,url):
-    async with sem:
         try:
-            async with session.get(url,allow_redirects=True,timeout=aiohttp.ClientTimeout(total=10)) as r:
-                if r.status in (200,301,302) and "login" not in str(r.url).lower():
-                    return url
-        except:
-            return None
+            r = requests.get(
+                profile_url,
+                headers=HEADERS,
+                timeout=5,
+                allow_redirects=True
+            )
 
-async def _runner(username,use_tor):
-    key=hashlib.sha1((username+str(use_tor)).encode()).hexdigest()
-    if key in CACHE and time.time()-CACHE[key]["t"]<TTL:
-        return CACHE[key]["d"]
-    found={}
-    sem=asyncio.Semaphore(50 if use_tor else 120)
-    proxy="socks5h://127.0.0.1:9050" if use_tor and _tor_available() else None
-    conn=aiohttp.TCPConnector(limit=50 if use_tor else 120,ssl=False)
-    async with aiohttp.ClientSession(connector=conn) as s:
-        tasks=[]
-        for m in _mutations(username):
-            for u in ALL_URLS:
-                tasks.append(_check(s,sem,u.replace("{}",m)))
-        for r in await asyncio.gather(*tasks):
-            if r:
-                dom=r.split("/")[2]
-                for name,(bases,w) in PLATFORMS.items():
-                    if any(b.replace("https://","").split("/")[0] in dom for b in bases):
-                        found.setdefault(name,{"weight":w,"urls":[]})["urls"].append(r)
-    CACHE[key]={"t":time.time(),"d":found}
-    return found
+            if r.status_code == 200:
+                results[site] = {
+                    "weight": info["weight"],
+                    "urls": [profile_url]
+                }
 
-def find_socials(username,tor=False):
-    return asyncio.run(_runner(username,tor))
+        except requests.RequestException:
+            continue
+
+    return results
